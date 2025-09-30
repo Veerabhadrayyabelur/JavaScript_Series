e4066de22852afac2ebb032be1fa342b0aac47a3
Veerabhadrayyabelur <160339126+Veerabhadrayyabelur@users.noreply.github.com>
2025-09-30 10:47:49 +0530

    Add files via upload

---
+++ Nums and Math.js
@@ -0,0 +1,29 @@
+// NUMBERS IN JAVASCRIPT
+
+// console.log("veer");
+
+
+// const veer = new Number(100);
+// console.log(veer);
+
+
+// const  a = 999.3;
+// console.log(a.toPrecision(1));
+
+// const b = 1000000;
+// console.log(b.toLocaleString());
+// console.log(b.toLocaleString('en-IN'));
+
+
+// MATH IN JAVASCRIPT
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(789.2));/
+// console.log(Math.sinh(1));
+// console.log(Math.min(1,2,3,4,5,1));
+// console.log(Math.min(1,2,3,4,5,1));
+
+
+console.log(Math.floor(Math.random()*10) +1);
