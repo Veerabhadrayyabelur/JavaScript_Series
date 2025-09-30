17498a2adfbd2b933c4916deaf206c2fe0a6979f
Veerabhadrayyabelur <160339126+Veerabhadrayyabelur@users.noreply.github.com>
2025-09-30 11:10:45 +0530

    Add files via upload

---
+++ date_time.js
@@ -0,0 +1,17 @@
+// Date and Time in Javascript
+
+
+let d = new Date();
+// console.log(d);
+// console.log(d.toString());
+// console.log(d.getMonth());
+// console.log(d.toString());
+// console.log(d.toLocaleString());
+// console.log(d.toDateString());
+
+// let a = new Date(2023,0,5,5,4)
+// console.log(a.toLocaleString())
+
+
+let a = Date.now()
+console.log(a/1000);
