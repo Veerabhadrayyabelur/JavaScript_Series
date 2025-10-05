8c3dbd8c5850b78d27ebc8fcde851722aa06476f
Veerabhadrayyabelur <veerabhadrayyabelur2k3@gmail.com>
2025-10-05 22:25:11 +0530

    series

---
+++ Objects.js
@@ -0,0 +1,16 @@
+// Object literals
+const  Veer = {
+    name : 'Veeresh',
+    age : '18',
+    location : 'beng', 
+    email : 'veer',
+    isloggedin : true
+};
+
+
+
+console.log(Veer["name"]);
+
+
+Veer.name = " saemy";
+console.log(Veer);
