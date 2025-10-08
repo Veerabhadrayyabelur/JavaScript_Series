155d7beabf032d756f7feba509cc722a2cbeef36
Veerabhadrayyabelur <veerabhadrayyabelur2k3@gmail.com>
2025-10-08 10:03:25 +0530

    practice

---
+++ Functions.js
@@ -0,0 +1,51 @@
+function Greet() {
+    console.log("Veer");
+    console.log("Vr");
+    console.log("B");
+    console.log("CC");
+    
+}
+
+// Greet()
+
+
+// function addNumbers(num1, num2){
+
+// console.log(num1 + num2);
+
+
+// }
+// addNumbers(1,2);
+// addNumbers(1,"3");
+// addNumbers(1,"a0");
+// addNumbers(1,null);
+
+
+// const result =  addNumbers(4,8);
+// console.log(result);
+
+
+
+function addcartitem(val1, val2 ,...num1) {
+    return num1 
+
+
+}
+// console.log(addcartitem(1,2,3,4));
+
+
+const product = {
+    name : "Chai",
+    price : 12
+    
+}
+
+
+function print(dataproduct){
+  console.log(`${dataproduct.name} ${dataproduct.price}`);
+    
+}
+print(product);
+
+
+
