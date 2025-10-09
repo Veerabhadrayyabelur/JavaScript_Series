e78ce0cecb94d3a3349a0ba679822a569aa6bfda
Veerabhadrayyabelur <veerabhadrayyabelur2k3@gmail.com>
2025-10-09 09:03:22 +0530

    global/local

--- Local-Global.js
+++ Local-Global.js
@@ -1,7 +1,24 @@
+// scope levels
+// block scope and global scope 
 let a = 20;
 const b = 20;
 var c = 35;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one ( ){
+    const name = " Web"
+
+    function two (){
+        const web = " VsCode"
+        console.log(name);
+
+         console.log(web);
+    }
+   
+    two()
+    
+}
+one()
